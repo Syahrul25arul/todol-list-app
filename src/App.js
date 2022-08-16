002b49b9388e61887ed050157d7da15fce4f5d78
syahrul25arul <syahrul25arul@gmail.com>
2022-08-16 17:40:46 +0700

    activate app

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
 
 	return (
 		<div className="App">
-			{/* <Main /> */}
-			<input type="text" placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
-			<input type="text" placeholder="name2" value={name} />
+			<Main />
 		</div>
 	);
 }
